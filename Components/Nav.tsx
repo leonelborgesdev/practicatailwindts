e5af5030d18e7fe04b03fa4977076d29b65af5c9
leonelborgesdev <leonel03t@gmail.com>
2024-05-26 22:38:10 -0400

    nav nav-link

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
-        <div>
-            <h1>
+        <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
+            <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
                 WEB
-                <span>DEV</span>
+                <span className='text-yellow-300'>DEV</span>
             </h1>
-            <div>Home</div>
-            <div>SERVICES</div>
-            <div>ABOUT</div>
-            <div>PROJECT</div>
-            <div>BLOG</div>
-            <div>CONTACT</div>
+            <div className='nav-link'>Home</div>
+            <div className='nav-link'>SERVICES</div>
+            <div className='nav-link'>ABOUT</div>
+            <div className='nav-link'>PROJECT</div>
+            <div className='nav-link'>BLOG</div>
+            <div className='nav-link'>CONTACT</div>
         </div>
     </div>
   )
