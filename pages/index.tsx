2789ac4a9c7a3562b29df794571855ecf86b69a2
leonelborgesdev <leonel03t@gmail.com>
2024-05-27 23:40:20 -0400

    navmobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
+import MobileNav from '@/Components/MobileNav';
 import Nav from '@/Components/Nav';
-import React from 'react'
+import React, { useState } from 'react'
 
 const HomePage = () => {
+
+  const [nav, setNav]= useState(false);
+  const openNav=()=>setNav(true)
+  const closeNav=()=>setNav(false)
+
   return (
     <div className="overflow-x-hidden">
       <div>
+        <MobileNav/>
         <Nav/>
       </div>
     </div>
