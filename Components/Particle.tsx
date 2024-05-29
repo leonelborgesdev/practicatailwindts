4a7db528252c5487ef6ae177d8c4ecf5ce0c460b
leonelborgesdev <leonel03t@gmail.com>
2024-05-28 23:01:32 -0400

    react-tsparticles

--- Components/Particle.tsx
+++ Components/Particle.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
 
 const Particle = () => {
   return (
