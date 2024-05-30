9bc204ed07a86afb01a97afa2ff53164024547f3
leonelborgesdev <leonel03t@gmail.com>
2024-05-30 15:03:47 -0400

    watch video

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownTrayIcon } from "@heroicons/react/20/solid";
+import { ArrowDownTrayIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 import Particle from "./Particle";
 import TextEffect from "./TextEffect";
 import Image from "next/image";
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className={`h-[88vh] bg-[url('/images/banner.jpeg')] bg-cover bg-center`}>
       <Particle />
-      <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols2 gap-[3rem] h-[100%] items-center'>
+      <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
         <div>
           <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
             HI, I'M <span className='text-yellow-400'>LEONEL</span>
@@ -22,6 +22,12 @@ const Hero = () => {
             <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
               <p>Dowload CV</p>
               <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-blank"/>
+            </button>
+            <button className="flex items-center space-x-2">
+              <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5]"/>
+              <p className="text-[20px] font-semibold text-white">
+                Watch The Video
+              </p>
             </button>
           </div>
         </div>
