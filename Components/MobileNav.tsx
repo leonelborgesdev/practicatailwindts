8ff770ba89500db6de2363015b45c318427d3c29
leonelborgesdev <leonel03t@gmail.com>
2024-05-29 22:30:17 -0400

    mobilenav

--- Components/MobileNav.tsx
+++ Components/MobileNav.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 interface Props{
     nav: boolean;
-    closeNav: ()=> void;
+    closeNav: () => void;
 }
 
 const MobileNav = ( {nav, closeNav}: Props) => {
-  const navAnimation = nav?'translate-x-0':'translate-x-[100%]';
+  const navAnimation = nav ? 'translate-x-0' : 'translate-x-[-100%]';
   return (
     <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}>
         <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
@@ -18,7 +18,9 @@ const MobileNav = ( {nav, closeNav}: Props) => {
             <div className='nav-link-mobile'>BLOG</div>
             <div className='nav-link-mobile'>CONTACT</div>
         </div>
-        <div onClick={closeNav} className='absolute cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400'>
+        <div 
+        onClick={closeNav} 
+        className='absolute cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400'>
             <XMarkIcon/>
         </div>
     </div>
