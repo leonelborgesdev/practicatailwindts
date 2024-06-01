be2b95fe4d735cdcf88ad87c662586a736ad568e
leonelborgesdev <leonel03t@gmail.com>
2024-05-31 21:37:16 -0400

    frontend service

--- Components/Services.tsx
+++ Components/Services.tsx
@@ -8,7 +8,20 @@ const Services = () => {
             My <span className='text-yellow-400'>Services</span>
         </p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-whites'>
-            <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'><div><CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]'/></div></div>
+            <div className='bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]'>
+                <div>
+                    <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3fae8]'/>
+                    <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>
+                        Frontend
+                    </h1>
+                    
+                    <p className="text-[15px] text-[#d3d2d2] font-normal">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                        molestias! Sed iste enim accusamus veritatis vitae, doloribus
+                        aliquam et totam cumque, neque, quos in aut.
+                    </p>
+                </div>
+            </div>
             <div></div>
             <div></div>
         </div>
