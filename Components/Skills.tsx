427a33dbbbf7e481e64ada0bc6c101e55b71d059
leonelborgesdev <leonel03t@gmail.com>
2024-06-03 23:24:49 -0400

    skill1

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -12,9 +12,16 @@ const Skills = () => {
         {/*------Skills------ */}
         <div className='w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center'>
           <div>
-            <SkillsItems/>
-            <SkillsItems/>
-            <SkillsLanguaje/>
+            <SkillsItems title="React Developer" year="2014 - 2018"/>
+            <SkillsItems title="MERN Stack Developer" year="2016 - 2020"/>
+            <SkillsLanguaje 
+            skill1="html" 
+            skill2="css" 
+            skill3="javascript" 
+            leve1="w-[91%]" 
+            level2="w-[88%]" 
+            level3="w-[80%]"
+            />
           </div>
           <div>
             <SkillsItems/>
