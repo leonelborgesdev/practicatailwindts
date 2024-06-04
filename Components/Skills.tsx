a8385b17b2b97e1dcf719a706c7cc09b1d9773cc
leonelborgesdev <leonel03t@gmail.com>
2024-06-03 23:17:21 -0400

    skills language

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import SkillsItems from './SkillsItems'
+import SkillsLanguaje from './SkillsLanguaje'
 
 const Skills = () => {
   return (
@@ -7,6 +9,19 @@ const Skills = () => {
         <h1 className='heading'>
             Education & <span className='text-yellow-400'>Skill</span>
         </h1>
+        {/*------Skills------ */}
+        <div className='w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center'>
+          <div>
+            <SkillsItems/>
+            <SkillsItems/>
+            <SkillsLanguaje/>
+          </div>
+          <div>
+            <SkillsItems/>
+            <SkillsItems/>
+            <SkillsLanguaje/>
+          </div>
+        </div>
     </div>
   )
 }
